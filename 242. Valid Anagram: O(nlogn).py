6c345677391af6b0e80170ebb127ee048b644797
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-27 14:58:58 +0300

    Create 242. Valid Anagram: O(nlogn).py

---
+++ 242. Valid Anagram: O(nlogn).py
@@ -0,0 +1,9 @@
+# 242. Valid Anagram
+# https://leetcode.com/problems/valid-anagram/description/
+
+# Trivial solution using built-in sorting O(s log s + t log t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sorted = ''.join(sorted(s))
+        t_sorted = ''.join(sorted(t))
+        return s_sorted == t_sorted
