b9adb49ef3a7cb061e6f3434fe02dbb641fa4281
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-27 18:57:16 +0300

    Create 49. Group Anagrams: O(m*n).py

---
+++ 49. Group Anagrams: O(m*n).py
@@ -0,0 +1,20 @@
+# 49. Group Anagrams
+# https://leetcode.com/problems/group-anagrams/description/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+
+        for s in strs:
+            # Counter for each letter from "a" to "z":
+            count = [0] * 26
+
+            # Increase the count for each letter (maps ASCII value to array index):
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            # Add string as value with key of letter count mapping:
+            map.setdefault(tuple(count), []).append(s)
+
+        # Returns the group of anagrams as array of all hashmap values:
+        return map.values()
