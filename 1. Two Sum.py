de584384749cb81c7b36b6cac292f81046929caf
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-27 14:03:34 +0300

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,7 +1,7 @@
 # 1. Two Sum
 # https://leetcode.com/problems/two-sum/
 
-# Trivial O(n^2) algorithm to return two indices that sum up to target:
+# Brute force O(n^2) algorithm to return two indices that sum up to target:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
