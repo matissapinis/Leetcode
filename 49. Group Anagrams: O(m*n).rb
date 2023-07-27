3dd6a2c771fead52a7d14e7d3cd0673fc9585b73
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-27 18:59:55 +0300

    Create 49. Group Anagrams: O(m*n).rb

---
+++ 49. Group Anagrams: O(m*n).rb
@@ -0,0 +1,25 @@
+# 49. Group Anagrams
+# https://leetcode.com/problems/group-anagrams/description/
+
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+    map = {}
+
+    strs.each do |s|
+        # Counter for each letter from "a" to "z":
+        count = Array.new(26, 0)
+
+        # Increase the count for each letter:
+        s.each_char do |c|
+            count[c.ord - 'a'.ord] += 1
+        end
+
+        # Add string as value with key of letter count mapping:
+        map[count] ||= []
+        map[count] << s
+    end
+
+    # Returns the group of anagrams as array of all hashmap values:
+    map.values
+end
