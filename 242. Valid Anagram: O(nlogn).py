f9548ca33ab9052aba3dc471be02277327d55180
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-27 16:47:00 +0300

    Update 242. Valid Anagram: O(nlogn).py

--- 242. Valid Anagram: O(nlogn).py
+++ 242. Valid Anagram: O(nlogn).py
@@ -1,7 +1,7 @@
 # 242. Valid Anagram
 # https://leetcode.com/problems/valid-anagram/description/
 
-# Trivial solution using built-in sorting O(s log s + t log t):
+# Trivial solution using built-in sorting in O(s log s + t log t):
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_sorted = ''.join(sorted(s))
