ab64d6658130e17be6d542335e71e563b30e93b6
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 13:39:44 +0300

    Create 11. Container With Most Water.rb

---
+++ 11. Container With Most Water.rb
@@ -0,0 +1,33 @@
+# 11. Container With Most Water
+# https://leetcode.com/problems/container-with-most-water/description/
+
+# @param {Integer[]} height
+# @return {Integer}
+
+# O(n) algorithm for largest water container area in array using two indices:
+def max_area(height)
+    # Initialize left and right index for going through array from each side:
+    idx_L, idx_R = 0, height.length - 1
+
+    max_area = 0
+
+    # Go through the array until indices meet in the middle:
+    while idx_L < idx_R
+        # 2-D water container area is width (index difference) by height (smallest of the two):
+        current_area = (idx_R - idx_L) * [height[idx_L], height[idx_R]].min
+
+        # Change the area of largest container if new container is largest:
+        max_area = [current_area, max_area].max
+
+        # Move left index left if left height is smaller:
+        if height[idx_L] < height[idx_R]
+            idx_L += 1
+        # Move right index right otherwise: 
+        else
+            idx_R -= 1
+        end
+    end
+
+    # Return area of largest water container:
+    return max_area
+end
