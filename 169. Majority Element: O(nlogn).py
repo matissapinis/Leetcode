25affd596b03a7f5dcb9444c8a174bb1a4d3ae26
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 21:56:04 +0300

    Create 169. Majority Element: O(nlogn).py

---
+++ 169. Majority Element: O(nlogn).py
@@ -0,0 +1,17 @@
+# 169. Majority Element
+# https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+# O(nlogn) algorithm for majority element in array using built-in sorting on hashmap:
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        map = {}
+
+        # Creates a hashmap how many instances each number has:
+        for i in range(len(nums)):
+            map[nums[i]] = map.get(nums[i], 0) + 1
+
+        # Sorts key-value pairs by values (biggest to lowest) and returns largest value:
+        max_element = sorted(map.items(), key=lambda x: x[1], reverse=True)[0]
+        
+        # Returns key from topmost key-value pair by value (i.e., majority element with largest count):
+        return max_element[0]
