396d8ef9cf75a1fc2e6b214c4eb23b6ccc515beb
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 21:11:07 +0300

    Update 74. Search a 2D Matrix: O(logm+logn).rb

--- 74. Search a 2D Matrix: O(logm+logn).rb
+++ 74. Search a 2D Matrix: O(logm+logn).rb
@@ -11,25 +11,25 @@ def search_matrix(matrix, target)
     m, n = matrix.length, matrix[0].length
     top, bottom = 0, m - 1
 
-    # Binary search for the row that contains target:
+    # Binary search for the row that may contain target:
     while top <= bottom
         # Take the middle row of current binary search interval, dividing it in halves:
         # While row = (top + bottom) / 2 is more natural, it's prone to integer overflow.
         row = top + (bottom - top) / 2
 
-        # If target is on bottom-half of rows, update top bound greater than mid row:
+        # If target may be in bottom-half of rows, update top bound greater than mid row:
         if matrix[row][-1] < target
             top = row + 1
-        # If target is on top-half of rows, update bottom bound less than mid row:
+        # If target may be in top-half of rows, update bottom bound less than mid row:
         elsif matrix[row][0] > target
             bottom = row - 1
-        # Break if row with target is found:
+        # Break if potential row with target is found:
         else 
             break
         end
     end
 
-    # Return false if no row with target was found:
+    # Return false if no potential row with target was found:
     return false if not (top <= bottom)
 
     # Define the column initial bounds to binary search over found row:
