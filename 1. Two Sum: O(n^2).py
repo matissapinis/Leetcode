8d1ce1fa7e143505587de250f1dbb001123b3311
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 10:45:18 +0300

    Update 1. Two Sum: O(n^2).py

--- 1. Two Sum: O(n^2).py
+++ 1. Two Sum: O(n^2).py
@@ -4,7 +4,7 @@
 # Brute force O(n^2) algorithm to return two indices that sum up to target:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Iterate through pairs of numbers (if equal skip, else check if sum to target):
+        # Iterate through pairs of numbers (if equal skip, else check if sums to target):
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i == j:
