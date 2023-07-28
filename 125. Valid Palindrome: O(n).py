4345a0c3d90b1d41053bc98386862958443c9c7e
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 11:05:19 +0300

    Update 125. Valid Palindrome: O(n).py

--- 125. Valid Palindrome: O(n).py
+++ 125. Valid Palindrome: O(n).py
@@ -1,6 +1,7 @@
 # 125. Valid Palindrome
 # https://leetcode.com/problems/valid-palindrome/description/
 
+# O(n) algorithm to check if string is palindromic:
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Convert string to lowercase alphanumeric characters only:
@@ -11,5 +12,6 @@ class Solution:
         for i in range(len(s) // 2):
             if s[i] != s[len(s) - i - 1]:
                 return False
-
+        
+        # Return true if no such characters were not equal:
         return True
