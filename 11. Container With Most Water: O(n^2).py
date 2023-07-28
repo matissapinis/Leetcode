7a8b20bae1874ecfde652ed3c5ba20888dff5fe4
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 12:47:44 +0300

    Create 11. Container With Most Water: O(n^2).py

---
+++ 11. Container With Most Water: O(n^2).py
@@ -0,0 +1,27 @@
+# 11. Container With Most Water
+# https://leetcode.com/problems/container-with-most-water/description/
+
+# Trivial O(n^2) algorithm for largest water container area in array (exceeds time limit):
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0 * 0
+
+        # Iterate through all pairs of container widths and heights:
+        for i in range(len(height)):
+            for j in range(len(height)):
+                # Water container width is difference between indices:
+                current_width = i - j
+                
+                # Water container height is the smallest of two heights:
+                current_height = min(height[i], height[j])
+                
+                # 2-D container area is width multiplied by height:
+                current_area = current_width * current_height
+                
+                # Change the area of largest container if new container is largest:
+                max_area = max(current_area, max_area)
+        
+        # Return the area of the largest container:
+        return max_area
+
+
