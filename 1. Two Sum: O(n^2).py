67eb27d837ebc7733efddfd97ad8effee5663349
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 10:34:41 +0300

    Update 1. Two Sum: O(n^2).py

--- 1. Two Sum: O(n^2).py
+++ 1. Two Sum: O(n^2).py
@@ -1,9 +1,10 @@
 # 1. Two Sum
-# https://leetcode.com/problems/two-sum/
+# https://leetcode.com/problems/two-sum/description/
 
 # Brute force O(n^2) algorithm to return two indices that sum up to target:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Iterate through pairs of numbers (if equal skip, else check if sum to target):
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i == j:
