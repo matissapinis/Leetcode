706528e6d56474aab792c8bc5dbe8fc492f8fe00
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 11:28:29 +0300

    Update 242. Valid Anagram: O(n).rb

--- 242. Valid Anagram: O(n).rb
+++ 242. Valid Anagram: O(n).rb
@@ -4,10 +4,13 @@
 # @param {String} s
 # @param {String} t
 # @return {Boolean}
+
+# O(n) algorithm if string has anagram using hashmaps:
 def is_anagram(s, t)
+    # Cannot be anagram if lengths of strings are different:
     return false if s.length != t.length
 
-    # Build a hashmap of each character in string, then compare hashmaps in O(s + t):
+    # Build a hashmap of each character in string, then compare hashmaps in O(|s|):
     count_s, count_t = {}, {}
     for i in 0...s.length
         # Increment character counter by one (with 'fetch' method if map doesn't exist):
@@ -15,9 +18,11 @@ def is_anagram(s, t)
         count_t[t[i]] = count_t.fetch(t[i], 0) + 1
     end
 
+    # Not an anagram if character counts aren't equal between strings:
     count_s.each do |c, _|
         return false if count_s[c] != count_t.fetch(c, 0)
     end
 
+    # Is an anagram if no character counts were different:
     true
 end
