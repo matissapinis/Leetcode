b38523abb6d1d1f023599e55e2835da9fd8ba7ac
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 11:31:33 +0300

    Update 242. Valid Anagram: O(nlogn).rb

--- 242. Valid Anagram: O(nlogn).rb
+++ 242. Valid Anagram: O(nlogn).rb
@@ -5,9 +5,12 @@
 # @param {String} t
 # @return {Boolean}
 
-# Trivial solution using built-in sorting in O(s log s + t log t):
+# Trivial algorithm if string has anagram using built-in sorting in O(s*log(s) + t*log(t)):
 def is_anagram(s, t)
+    # Sort each string in O(nlogn):
     s_sorted = s.chars.sort.join
     t_sorted = t.chars.sort.join
+
+    # Is anagram if sorted strings are equal:
     s_sorted == t_sorted
 end
