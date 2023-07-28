1137fe5fdb78a2f093bb292206c5ff4f3ea72648
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 14:55:41 +0300

    Create 704. Binary Search: O(logn).rb

---
+++ 704. Binary Search: O(logn).rb
@@ -0,0 +1,39 @@
+# 704. Binary Search
+# https://leetcode.com/problems/binary-search/description/
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+
+# Binary search in O(logn):
+def search(nums, target)
+    # Define the initial bounds of the binary search:
+    l, r = 0, nums.length - 1
+
+    while l <= r
+        # Take the middle of current binary search interval, dividing it in halves:
+        # While m = (l + r) / 2 is more natural, it's prone to integer overflow.
+        m = l + (r - l) / 2
+        
+        # If target is on right half of interval, update left bound greater than midpoint:
+        if nums[m] < target
+            l = m + 1
+        # If target is on left half of interval, update right bound less than midpoint:
+        elsif nums[m] > target
+            r = m - 1
+        # Return the index if target is found:
+        else
+            return m
+        end
+    end
+
+    # Binary search doesn't find target:
+    -1
+end
+
+
+
+
+
+        
+
