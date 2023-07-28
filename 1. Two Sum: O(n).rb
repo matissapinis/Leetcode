fa5f2b9af88d643dbcae0b8e2375802f2643e51d
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 10:33:12 +0300

    Update 1. Two Sum: O(n).rb

--- 1. Two Sum: O(n).rb
+++ 1. Two Sum: O(n).rb
@@ -1,36 +1,30 @@
 # 1. Two Sum
-# https://leetcode.com/problems/two-sum/
+# https://leetcode.com/problems/two-sum/description/
     
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer[]}
 
-# Brute force O(n^2) algorithm timed out, so here's a O(n) hashmap solution suggested by ChatGPT (GPT-4):
+# Brute force O(n^2) algorithm timed out, so here's a O(n) hashmap solution:
 def two_sum(nums, target)
-    # Initialize an empty hash map. This will store each number in the array as a key,
-    # and the corresponding value will be the index of that number in the array.
+    # Initialize empty hashmap to store each number as key with value as index plus one of that number:
     map = {}
 
-    # The 'each_with_index' method is used to iterate through each element in the array,
-    # along with its index. The variables 'num' and 'i' represent the current element and
-    # its index, respectively.
+    # Iterate through each element in the list, along with its index:
     nums.each_with_index do |num, i|
-        # Calculate the complement of the current number. If two numbers add up to the target,
-        # one number is the complement of the other (target minus the number).
+        # If two numbers add up to target, one number is the complement of the other (target minus number):
         complement = target - num
 
-        # Check if the complement is in the hash map. The 'key?' method checks if a key exists in the hash.
-        # If the complement exists, it means we have found two numbers that add up to the target.
+        # Check if complement is in dictionary (if yes, we have found two numbers that add up to target):
         if map.key?(complement)
-            # Return the indices of the two numbers. 'map[complement]' gets the index of the complement,
-            # and 'i' is the index of the current number.
+            # Return indices of the two numbers ('map[complement]' – complement index, 'i + 1' – current number):
             return [map[complement], i]
         end
 
-        # If the complement is not in the hash map, add the current number and its index to the hash map.
+        # If complement is not in dictionary, add current number and its index (added by one):
         map[num] = i
     end
 
-    # If no two numbers add up to the target, return an empty array.
+    # If no two numbers add up to target, return empty list:
     []
 end
