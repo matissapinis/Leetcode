d4115902bc1e6485b2e36c075adc549e69b469a6
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 11:21:52 +0300

    Update 217. Contains Duplicate: O(nlogn).py

--- 217. Contains Duplicate: O(nlogn).py
+++ 217. Contains Duplicate: O(nlogn).py
@@ -1,13 +1,16 @@
 # 217. Contains Duplicate
 # https://leetcode.com/problems/contains-duplicate/description/
 
-# Trivial solution using built-in sorting in O(nlogn):
+# Trivial O(nlogn) algorithm for duplicates in array using built-in sorting:
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Sort number array in O(nlogn):
         nums.sort()
 
+        # Iterate through adjacent pairs of sorted numbers to check if they're equal:
         for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:
                 return True
-                
+
+        # No duplicates found if none of pairs were equal:
         return False
