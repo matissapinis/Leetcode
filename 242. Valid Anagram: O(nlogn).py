8e433b8355c6c677a605cc8d671cc9bbb7e8581c
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-28 11:31:01 +0300

    Update 242. Valid Anagram: O(nlogn).py

--- 242. Valid Anagram: O(nlogn).py
+++ 242. Valid Anagram: O(nlogn).py
@@ -1,9 +1,12 @@
 # 242. Valid Anagram
 # https://leetcode.com/problems/valid-anagram/description/
 
-# Trivial solution using built-in sorting in O(s log s + t log t):
+# Trivial algorithm if string has anagram using built-in sorting in O(s*log(s) + t*log(t)):
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Sort each string in O(nlogn):
         s_sorted = ''.join(sorted(s))
         t_sorted = ''.join(sorted(t))
+
+        # Is anagram if sorted string are equal:
         return s_sorted == t_sorted
