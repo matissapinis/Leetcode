6c9b6b1c6d7ee240ec6c9d8503ba701e070eed56
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-30 19:18:34 +0300

    Create 33. Search in Rotated Sorted Array: O(logn).py

---
+++ 33. Search in Rotated Sorted Array: O(logn).py
@@ -0,0 +1,37 @@
+# 33. Search in Rotated Sorted Array
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Define the initial bounds of the binary search on whole array:
+        L, R = 0, len(nums) - 1
+
+        # The loop continues until the left index is greater than the right index:
+        while L <= R:
+            # Take the middle of current binary search interval, dividing it in halves:
+            # While M = (L + R) // 2 is more natural, it's prone to integer overflow.
+            M = L + (R - L) // 2
+
+            # If midpoint already is target, return it:
+            if nums[M] == target:
+                return M
+
+            # If the left half of array is sorted:
+            if nums[L] <= nums[M]:
+                # If target is in sorted left half (between left index and midpoint), search it by updating right bound less than midpoint:
+                if nums[L] <= target and target < nums[M]:
+                    R = M - 1
+                # Otherwise, target must be in right half, so update left bound greater than midpoint:
+                else:
+                    L = M + 1
+            # If the right half of array is sorted:
+            else:
+                # If target is in sorted right half (between midpoint and right index), search it by updating left bound greater than midpoint:
+                if nums[M] < target and target <= nums[R]: 
+                    L = M + 1
+                # Otherwise, the target must be in left half, update right bound less than midpoint:
+                else:
+                    R = M - 1
+
+        # Binary search doesn't find target:
+        return -1
