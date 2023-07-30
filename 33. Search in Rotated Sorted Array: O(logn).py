c3eefab0ec1fd37189c0a358c1e2f3c891579094
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-30 19:19:18 +0300

    Update 33. Search in Rotated Sorted Array: O(logn).py

--- 33. Search in Rotated Sorted Array: O(logn).py
+++ 33. Search in Rotated Sorted Array: O(logn).py
@@ -1,6 +1,7 @@
 # 33. Search in Rotated Sorted Array
 # https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
+# O(logn) algorithm for binary searching target in rotated sorted array:
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # Define the initial bounds of the binary search on whole array:
