9caf88f2332833bb3681afcb18eec518c7fd1b1e
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:36:38 +0300

    Update 153. Find Minimum in Rotated Sorted Array: O(logn).py

--- 153. Find Minimum in Rotated Sorted Array: O(logn).py
+++ 153. Find Minimum in Rotated Sorted Array: O(logn).py
@@ -5,20 +5,20 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         # Define the initial bounds of the binary search on the whole array:
-        L, R = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         # The loop continues until the left index is greater than the right index:
-        while L < R:
+        while l < r:
             # Take the middle of the current binary search interval, dividing it in halves:
-            # While M = (L + R) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
-            M = L + (R - L) // 2
+            # While m = (l + r) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
+            m = l + (r - l) // 2
 
             # If right bound is less than midpoint, minimum element is in right half, so update left bound:
-            if nums[M] > nums[R]:
-                L = M + 1
-            # Otherwise, minimum element is in left half (including M), so update right bound:
+            if nums[m] > nums[r]:
+                l = m + 1
+            # Otherwise, minimum element is in left half (including 'm'), so update right bound:
             else:
-                R = M
+                r = m
 
         # When bounds meet the minimum element is found:
-        return nums[L]
+        return nums[l]
