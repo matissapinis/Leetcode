f5bfe8af880fab0674bcb7fb600629a14612fb09
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:42:15 +0300

    Update 704. Binary Search: O(logn).py

--- 704. Binary Search: O(logn).py
+++ 704. Binary Search: O(logn).py
@@ -5,23 +5,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # Define the initial bounds of the binary search:
-        L, R = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         # The loop continues until the left index is greater than the right index:
-        while L <= R:
+        while l <= r:
             # Take the middle of current binary search interval, dividing it in halves:
-            # While M = (L + R) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
-            M = L + (R - L) // 2
+            # While m = (l + r) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
+            m = l + (r - l) // 2
 
             # If target is on left half of interval, update right bound less than midpoint:
-            if nums[M] > target:
-                R = M - 1
+            if nums[m] > target:
+                r = m - 1
             # If target is on right half of interval, update left bound greater than midpoint:
-            elif nums[M] < target:
-                L = M + 1
+            elif nums[m] < target:
+                l = m + 1
             # Return the index if target is found:
             else:
-                return M
+                return m
 
         # Binary search doesn't find target:
         return -1
