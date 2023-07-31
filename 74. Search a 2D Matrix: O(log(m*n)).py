ded2f14dcd53e193c891a58c1cf9c64718f2ce52
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:43:10 +0300

    Update 74. Search a 2D Matrix: O(log(m*n)).py

--- 74. Search a 2D Matrix: O(log(m*n)).py
+++ 74. Search a 2D Matrix: O(log(m*n)).py
@@ -6,23 +6,23 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # Define the initial bounds of the 2D binary search:
         m, n = len(matrix), len(matrix[0])
-        L, R = 0, m * n - 1
+        l, r = 0, m * n - 1
 
         # The loop continues until the left index is greater than the right index:
-        while L <= R:
+        while l <= r:
             # Take the middle of current binary search interval, dividing it in halves:
-            # While M = (L + R) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
-            M = L + (R - L) // 2
+            # While m = (l + r) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
+            m = l + (r - l) // 2
             
             # Flatten 2D matrix into 1D array by using mapping from 1D midpoint to 2D indices:
-            i, j = M // n, M % n
+            i, j = m // n, m % n
 
             # If target is on left 1D-half of interval, update right bound less than midpoint:
             if matrix[i][j] > target:
-                R = M - 1
+                r = m - 1
             # If target is on right 1D-half of interval, update left bound greater than midpoint:
             elif matrix[i][j] < target:
-                L = M + 1
+                l = m + 1
             # Return true if target is found:
             else:
                 return True
