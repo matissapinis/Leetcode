da6620aaccd4f4c06e762ad83f6422f29f9f2fef
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:39:39 +0300

    Update 33. Search in Rotated Sorted Array: O(logn).py

--- 33. Search in Rotated Sorted Array: O(logn).py
+++ 33. Search in Rotated Sorted Array: O(logn).py
@@ -5,34 +5,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # Define the initial bounds of the binary search on whole array:
-        L, R = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         # The loop continues until the left index is greater than the right index:
-        while L <= R:
+        while l <= r:
             # Take the middle of current binary search interval, dividing it in halves:
-            # While M = (L + R) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
-            M = L + (R - L) // 2
+            # While m = (l + r) // 2 is more natural, it's prone to integer overflow (in C++ and Java).
+            m = l + (r - l) // 2
 
             # If midpoint already is target, return it:
-            if nums[M] == target:
-                return M
+            if nums[m] == target:
+                return m
 
             # If left element less than or equal to midpoint, midpoint is in left half of array:
-            if nums[L] <= nums[M]:
+            if nums[l] <= nums[m]:
                 # If target is in sorted left half (between left bound and midpoint), search it by updating right bound less than midpoint:
-                if nums[L] <= target and target < nums[M]:
-                    R = M - 1
+                if nums[l] <= target and target < nums[m]:
+                    r = m - 1
                 # Otherwise, target must be in right half, so update left bound greater than midpoint:
                 else:
-                    L = M + 1
+                    l = m + 1
             # If the left element is greater than midpoint, midpoint is in right half of array:
             else:
                 # If target is in sorted right half (between midpoint and right bound), search it by updating left bound greater than midpoint:
-                if nums[M] < target and target <= nums[R]: 
-                    L = M + 1
+                if nums[m] < target and target <= nums[r]: 
+                    l = m + 1
                 # Otherwise, the target must be in left half, update right bound less than midpoint:
                 else:
-                    R = M - 1
+                    r = m - 1
 
         # Binary search doesn't find target:
         return -1
