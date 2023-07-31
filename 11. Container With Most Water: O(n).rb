57a30f4fbee6c0784266c850886e2b500fd3627a
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:40:28 +0300

    Update 11. Container With Most Water: O(n).rb

--- 11. Container With Most Water: O(n).rb
+++ 11. Container With Most Water: O(n).rb
@@ -7,24 +7,24 @@
 # O(n) algorithm for largest water container area in array using two indices:
 def max_area(height)
     # Initialize left and right index for going through array from each side:
-    idx_L, idx_R = 0, height.length - 1
+    l, r = 0, height.length - 1
 
     max_area = 0
 
     # Go through the array until indices meet in the middle:
-    while idx_L < idx_R
+    while l < r
         # 2-D water container area is width (index difference) by height (smallest of the two):
-        current_area = (idx_R - idx_L) * [height[idx_L], height[idx_R]].min
+        current_area = (r - l) * [height[l], height[r]].min
 
         # Update area of largest container if new container is largest:
         max_area = [max_area, current_area].max
 
         # Move left index right if left height is smaller:
-        if height[idx_L] < height[idx_R]
-            idx_L += 1
+        if height[l] < height[r]
+            l += 1
         # Move right index left otherwise: 
         else
-            idx_R -= 1
+            r -= 1
         end
     end
 
