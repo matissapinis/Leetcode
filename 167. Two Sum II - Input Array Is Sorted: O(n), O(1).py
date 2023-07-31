8b70cd5969fa4279d43d4ca921d0409c507bc98d
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:37:36 +0300

    Update 167. Two Sum II - Input Array Is Sorted: O(n), O(1).py

--- 167. Two Sum II - Input Array Is Sorted: O(n), O(1).py
+++ 167. Two Sum II - Input Array Is Sorted: O(n), O(1).py
@@ -5,22 +5,22 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         # Initialize left and right index for going through array from each side:
-        L, R = 0, len(numbers) - 1
+        l, r = 0, len(numbers) - 1
 
         # Go through the sorted array until indices meet in the middle:
-        while L < R:
+        while l < r:
             # Sum the smaller left number with larger right number:
-            current_sum = numbers[L] + numbers[R]
+            current_sum = numbers[l] + numbers[r]
 
             # If two-sum is smaller than target, try next left number larger:
             if current_sum < target:
-                L += 1
+                l += 1
             # If two-sum is larger than target, try next right number smaller:
             elif current_sum > target:
-                R -= 1
+                r -= 1
             # If target two-sum found, return number indices (incremented by one):
             else:
-                return [L + 1, R + 1]
+                return [l + 1, r + 1]
 
         # If no two numbers add up to target, return empty list:
         return []
