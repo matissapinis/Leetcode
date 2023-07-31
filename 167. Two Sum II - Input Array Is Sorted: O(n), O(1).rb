5c71792248d31838aa9a8b5279798ee287303b0e
Matīss Apinis <matiss.apinis@gmail.com>
2023-07-31 22:38:24 +0300

    Update 167. Two Sum II - Input Array Is Sorted: O(n), O(1).rb

--- 167. Two Sum II - Input Array Is Sorted: O(n), O(1).rb
+++ 167. Two Sum II - Input Array Is Sorted: O(n), O(1).rb
@@ -5,22 +5,22 @@
 # O(n) algorithm with O(1) space for two-sum using two indices:
 def two_sum(numbers, target)
     # Initialize left and right index for going through array from each side:
-    idx_L, idx_R = 0, numbers.length - 1
+    l, r = 0, numbers.length - 1
 
     # Go through the sorted array until indices meet in the middle:
-    while idx_L < idx_R
+    while l < r
         # Sum the smaller left number with larger right number:
-        current_sum = numbers[idx_L] + numbers[idx_R]
+        current_sum = numbers[l] + numbers[r]
 
         # If two-sum is smaller than target, try next left number larger:
         if current_sum < target
-            idx_L += 1
+            l += 1
         # If two-sum is larger than target, try next right number smaller:
         elsif current_sum > target
-            idx_R -= 1
+            r -= 1
         # If target two-sum found, return number indices (incremented by one):
         else
-            return [idx_L + 1, idx_R + 1]
+            return [l + 1, r + 1]
         end
     end
 
