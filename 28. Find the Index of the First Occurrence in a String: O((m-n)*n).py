dc359b7b2772c2a21832274dc43263aec1a5a179
Matīss Apinis <matiss.apinis@gmail.com>
2023-08-03 14:12:27 +0300

    Update 28. Find the Index of the First Occurrence in a String: O((m-n)*n).py

--- 28. Find the Index of the First Occurrence in a String: O((m-n)*n).py
+++ 28. Find the Index of the First Occurrence in a String: O((m-n)*n).py
@@ -1,7 +1,7 @@
 # 28. Find the Index of the First Occurrence in a String
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
 
-# Trivial O((m - n) * n) algorithm for finding index of first occuring substring:
+# Brute force O((m - n) * n) algorithm for finding index of first occuring substring:
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m = len(haystack)
