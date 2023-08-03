6514eef8986dbf34a12c1f5746fb6b25f6b43899
Matīss Apinis <matiss.apinis@gmail.com>
2023-08-03 11:19:09 +0300

    Update 27. Remove Element: O(n^2).py

--- 27. Remove Element: O(n^2).py
+++ 27. Remove Element: O(n^2).py
@@ -1,7 +1,7 @@
 # 27. Remove Element
 # https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
 
-# O(n^2) algorithm for removing element instances then returning size of remaining array:
+# Trivial O(n^2) algorithm for removing element instances then returning size of remaining array:
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         # As long as value in array, keep removing its instances:
