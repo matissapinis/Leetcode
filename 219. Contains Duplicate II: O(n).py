7ea984097ea23ccdee601ca6abd9f3139458559b
Matīss Apinis <matiss.apinis@gmail.com>
2023-08-04 20:30:45 +0300

    Update 219. Contains Duplicate II: O(n).py

--- 219. Contains Duplicate II: O(n).py
+++ 219. Contains Duplicate II: O(n).py
@@ -4,16 +4,16 @@
 # O(n) algorithm for checking if there are two distinct indices with absolute difference less or equal to k:
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        nums_dict = {}
+        last_index = {}
 
         # Iterate over array with hashmap for integer as key and its last seen index as value:
         for i in range(len(nums)):
             # Check if difference between last seen index and current index is less than or equal to k:
-            if nums[i] in nums_dict and i - nums_dict[nums[i]] <= k:
+            if nums[i] in last_index and i - last_index[nums[i]] <= k:
                 return True
             
             # If not, update last seen index of this integer:
-            nums_dict[nums[i]] = i
+            last_index[nums[i]] = i
 
         # Return false if there is no such pair of indices:
         return False
