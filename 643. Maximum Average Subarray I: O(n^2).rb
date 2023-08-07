f5eccc6f62f98f3d97fde9e4f00348c18e4f78c2
Matīss Apinis <matiss.apinis@gmail.com>
2023-08-07 14:15:48 +0300

    Create 643. Maximum Average Subarray I: O(n^2).rb

---
+++ 643. Maximum Average Subarray I: O(n^2).rb
@@ -0,0 +1,24 @@
+# 643. Maximum Average Subarray I
+# https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Float}
+
+# Brute force O(n^2) algorithm for maximum average value of contiguous k-subarrays (exceeds time limit):
+def find_max_average(nums, k)
+    # Initialize maximum average as smallest possible value:
+    max_average = -1.0/0 # Represents negative infinity in Ruby
+
+    # Iterate through all contiguous subarrays by slicing:
+    for i in 0..(nums.length - k)
+        # Take average as sum divided by count:
+        current_average = nums[i, k].sum.to_f / k
+
+        # Update maximum average if new average is greatest:
+        max_average = [max_average, current_average].max
+    end
+
+    # Return the maximum average value of all contiguous subarrays:
+    max_average
+end
