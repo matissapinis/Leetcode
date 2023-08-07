9e0ed767b3f78ff1335204c6f1187a4aaf33f22f
Matīss Apinis <matiss.apinis@gmail.com>
2023-08-07 11:54:35 +0300

    Update and rename 345. Reverse Vowels of a String: O(n^2).py to 345. Reverse Vowels of a String: O(n).py

--- 345. Reverse Vowels of a String: O(n).py
+++ 345. Reverse Vowels of a String: O(n).py
@@ -1,9 +1,12 @@
 # 345. Reverse Vowels of a String
 # https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
 
-# O(n^2) algorithm for reversing vowel order in string (since slicing takes O(n) time):
+# O(n) algorithm for reversing vowel order in string:
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        # Convert string to a list of characters for in-place modification:
+        s_list = list(s)
+
         # Initialize left and right index for going through array from each side:
         l, r = 0, len(s) - 1
 
@@ -13,23 +16,24 @@ class Solution:
 
         # Go through the sorted array until indices meet in the middle:
         while l < r:
-            # If both are vowels, swap them by slicing and move to next pair:
-            if is_vowel(s[l]) and is_vowel(s[r]):
-                placeholder_character = s[l]
-                s = s[:l] + s[r] + s[(l + 1):]
-                s = s[:r] + placeholder_character + s[(r + 1):]
+            # If both are vowels, swap them and move to next pair:
+            if is_vowel(s_list[l]) and is_vowel(s_list[r]):
+                s_list[l], s_list[r] = s_list[r], s_list[l]
                 l += 1
                 r -= 1
             # If left isn't a vowel, move left index right:
-            elif not is_vowel(s[l]) and is_vowel(s[r]):
+            elif not is_vowel(s_list[l]) and is_vowel(s_list[r]):
                 l += 1
             # If right isn't a vowel, move right index left:
-            elif is_vowel(s[l]) and not is_vowel(s[r]):
+            elif is_vowel(s_list[l]) and not is_vowel(s_list[r]):
                 r -= 1
             # If both aren't vowels, move to next pair
             else:
                 l += 1
                 r -= 1
+
+        # Convert character list back to string:
+        s = ''.join(s_list)
 
         # Return string with reversed vowel order:
         return s
